266b70e5fe17eff461157f4386a7c417268bbaa7
Anoopjain8 <anoopjain8@gmail.com>
2024-08-02 23:10:50 +0530

    import products from products.js

--- New folder/amazon/scripts/amazon.js
+++ New folder/amazon/scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js'
+import { cart } from '../data/cart.js'
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 products.forEach((product) => {
@@ -55,30 +56,31 @@ products.forEach((product) => {
     `;
 })
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-document.querySelectorAll('.js-add-to-cart-button').forEach((button ) => {
+document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-      if(productId === item.productId){
+      if (productId === item.productId) {
         matchingItem = item;
-    }})
-    if(matchingItem){
+      }
+    })
+    if (matchingItem) {
       matchingItem.quantity += 1;
-    }else{
-     cart.push({
-      productId: productId, 
-      quantity: 1
-     });
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
     }
 
     let cartQuantity = 0;
     cart.forEach((item) => {
       cartQuantity += item.quantity;
     })
-     
+
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
   });
