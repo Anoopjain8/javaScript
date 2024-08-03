46de266573aa0e6d39779b5b96be35d3afdb7720
Anoopjain8 <anoopjain8@gmail.com>
2024-08-03 16:48:35 +0530

    Delet products from html when clicking delete

--- New folder/amazon/scripts/checkout.js
+++ New folder/amazon/scripts/checkout.js
@@ -5,16 +5,16 @@ import { formatCurrency } from "./utils/money.js";
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
-    const productId = cartItem.productId; 
-    let matchingProduct;
-    products.forEach((product) => {
-        if(product.id === productId){
-            matchingProduct = product;
-        }
-    })
-    cartSummaryHTML += 
+  const productId = cartItem.productId;
+  let matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  })
+  cartSummaryHTML +=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -97,6 +97,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 });
